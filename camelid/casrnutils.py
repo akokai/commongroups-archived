8b62dd16b7c1b31b11f8fc52f2d613b695a56584
akokai <ak@kaios.net>
2017-03-07 11:33:28 -0800

    Actually include the right version of casrnutils module

--- camelid/casrnutils.py
+++ camelid/casrnutils.py
@@ -5,14 +5,11 @@
 
 """Functions for dealing with CAS Registry Numbers (CASRN)."""
 
-from __future__ import unicode_literals
-from builtins import str
-
 import re
 import numpy as np
 
 
-def validate(casrn, boolean=False):
+def validate(casrn, output='str'):
     """
     Check the validity of a CASRN using the check digit.
 
@@ -23,19 +20,21 @@ def validate(casrn, boolean=False):
     Parameters:
         casrn (str or int): The CASRN to validate.
             Non-numeric characters are ignored.
-        boolean (bool): Whether to return bool rather than string.
+        output (str): Type of result to return; either ``'str'``
+            (default) or ``'bool'``.
 
     Returns:
-        The CASRN as :class:`str`, with uniform CAS-style hypenation and no
-        leading/trailing spaces; or ``None`` if the CASRN is invalid.
+        If ``output='str'``, returns the CASRN as :class:`str`, with uniform
+        CAS-style hypenation and no leading/trailing spaces; or ``None`` if
+        the CASRN is invalid.
 
-        If ``boolean=True``, returns ``True`` or ``False``
+        If ``output='bool'``, returns ``True`` or ``False``
         depending on validity.
 
     Examples:
         >>> validate('50--00--0 ')  # No data cleaning needed
         '50-00-0'
-        >>> validate('50-00-5', boolean=True)
+        >>> validate('50-00-5', output='bool')
         False
     """
     casrn = str(casrn)
@@ -49,26 +48,37 @@ def validate(casrn, boolean=False):
         check_digit = sum(digits[:-1] * np.arange(1, len(nums))[::-1]) % 10
         valid = (check_digit == digits[-1])
 
-    if boolean:
+    if output == 'bool':
         return valid
     else:
         return '-'.join([nums[:-3], nums[-3:-1], nums[-1:]]) if valid else None
 
 
-def find_valid(string):
+def find(string, lenient=False, valid=True):
     """
     Find all valid CASRNs in a string.
 
     Parameters:
         string (str or unicode): The string to search.
+        lenient (bool): Try to find badly-formatted CASRNs too
+            (default `False`).
+        valid (bool): Find only valid CASRNs (default ``True``).
 
     Returns:
         A list of all valid CASRNs found, each as a hyphenated string.
 
-    Example:
-        >>> find_valid('Formaldehyde (50-00-0), copolymer with urea (57-13-6)')
+    Examples:
+        >>> find('Formaldehyde (50-00-0), copolymer with urea (57-13-6)')
         ['50-00-0', '57-13-6']
     """
-    casrn_regex = re.compile(r'(\d{2,7})[-–—](\d{2})[-–—]\d')
-    found = [validate(i.group()) for i in casrn_regex.finditer(string)]
-    return [x for x in found if x is not None]
+    if lenient:
+        casrn_rx = re.compile(r'(\d+)[-–—](\d+)[-–—]*\d*')
+    else:
+        casrn_rx = re.compile(r'(\d{2,7})[-–—](\d{2})[-–—]\d')
+
+    found = [i.group() for i in casrn_rx.finditer(string)]
+
+    if valid:
+        found = list(filter(None, [validate(x) for x in found]))
+
+    return found
